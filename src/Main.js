29240cf9c86273fff479ab00be8dc6be6bf975d9
Abhishek Vyas <vyas6@purdue.edu>
2018-05-22 14:45:10 -0400

    Style main using css in js

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,16 @@ import Sidebar from './Sidebar.js'
 import NoteList from './NoteList.js'
 import NoteForm from './NoteForm.js'
 
+const style = {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch',
+
+}
+
 const Main = () => {
     return(
-        <div className = "Main">
+        <div className = "Main" style = {style}>
             <Sidebar />
             <NoteList />
             <NoteForm />
